b3e010dc499e6f169d0b7da5bce08a7383aed9d7
spoonzhang <spoonzhang@tencent.com>
2023-02-07 19:01:09 +0800

    init

---
+++ Source/MHWords/MHWordsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MHWordsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHWORDS_API AMHWordsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
